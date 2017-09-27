f2e24869e07755f266446cbe3c4739de4a6c47f7
xjs-js <xjs-js@users.noreply.github.com>
2017-09-27 22:34:33 +0800

    Add files via upload

---
+++ AddTwo.asm
@@ -0,0 +1,15 @@
+; AddTwo.asm - adds two 32-bit integers.
+
+.386
+.model flat, stdcall
+.stack 4096
+ExitProcess PROTO, dwExitCode:DWORD
+
+.code
+main PROC
+  mov  eax, 5
+  add  eax, 6
+
+  INVOKE ExitProcess, 0
+main ENDP
+END main
